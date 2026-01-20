1a65ca39bd6322b88e7b3ae902c7d071c2a88cd2
Bozhidar Zagorov <bozhidarzagorov@gmail.com>
2026-01-20 18:32:42 +0200

    some pages have styles now

--- recipe-app/src/App.jsx
+++ recipe-app/src/App.jsx
@@ -3,6 +3,7 @@ import Home from './pages/Home'
 import Login from './pages/Login'
 import Register from './pages/Register'
 import Recipes from './pages/Recipes'
+import CreateRecipe from './pages/CreateRecipe'
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
       <Route path="/recipes" element={<Recipes />} />
+      <Route path="/create" element={<CreateRecipe />} />
     </Routes>
   )
 }
--- recipe-app/src/pages/Recipes.jsx
+++ recipe-app/src/pages/Recipes.jsx
@@ -1,10 +1,11 @@
 import { useEffect, useState } from 'react'
 import { supabase } from '../services/supabase'
 import RecipeCard from '../components/RecipeCard'
-import RecipeForm from '../components/RecipeForm'
+import { useNavigate } from 'react-router-dom'
 
 export default function Recipes() {
   const [recipes, setRecipes] = useState([])
+  const navigate = useNavigate()
 
   const fetchRecipes = async () => {
     const { data, error } = await supabase
@@ -20,12 +21,32 @@ export default function Recipes() {
   }, [])
 
   return (
+    
     <div>
-      <h1>🍲 Recipes</h1>
-      <RecipeForm refresh={fetchRecipes} />
-      {recipes.map((recipe) => (
-        <RecipeCard key={recipe.id} recipe={recipe} refresh={fetchRecipes} />
-      ))}
+       <div className="w-screen flex flex-col items-center mb-8 px-4">
+      {/* Big header */}
+      <div className="text-5xl sm:text-6xl font-bold text-white mb-6">
+        🍲 Recipes
+      </div>
+          <div className="w-screen bg-white/60 backdrop-blur-md p-6 rounded-lg flex flex-col items-center">
+            {/* Header + Button */}
+            <div className="mb-6 text-center">
+              <button
+                onClick={() => navigate('/create')}
+                className="bg-yellow-500 hover:bg-yellow-600 text-white font-semibold px-6 py-3 rounded-lg text-lg transition"
+              >
+                Add a Recipe
+              </button>
+            </div>
+
+            {/* Recipe Grid */}
+            <div className="w-full grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+              {recipes.map((recipe) => (
+                <RecipeCard key={recipe.id} recipe={recipe} refresh={fetchRecipes} />
+              ))}
+            </div>
+          </div>
+      </div>
     </div>
   )
 }
