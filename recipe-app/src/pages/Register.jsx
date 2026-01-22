514e95716e2c0c6fe9aad7fe0a04888bbb485997
Bozhidar Zagorov <bozhidarzagorov@gmail.com>
2026-01-22 14:44:47 +0200

    register page style

--- recipe-app/src/pages/Register.jsx
+++ recipe-app/src/pages/Register.jsx
@@ -30,24 +30,84 @@ export default function Register() {
 }
 
   return (
-    <form onSubmit={handleRegister}>
-      <h2>Register</h2>
+    <div className="flex min-h-full flex-col justify-center lg:px-8">
+      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+        <img
+          className="mx-auto h-10 w-auto"
+          src="https://res.cloudinary.com/dbleq6bwe/image/upload/v1769081501/logo_cooking_eno9rk.png"
+          alt="Your Company"
+        />
+        <h2 className="mt-10 text-center text-2xl font-bold tracking-tight text-white">
+          Regiser your account
+        </h2>
+      </div>
 
-      <input
-        placeholder="Email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
+      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+        <form onSubmit={handleRegister} className="space-y-6">
+          <div>
+            <label htmlFor="email" className="block text-sm font-medium text-gray-100">
+              Email address
+            </label>
+            <div className="mt-2">
+              <input
+                id="email"
+                name="email"
+                type="email"
+                required
+                autoComplete="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                className="block w-full rounded-md bg-white/50 px-3 py-1.5 text-base text-white placeholder:text-gray-500 outline-1 -outline-offset-1 outline-white/10 focus:outline-2 focus:-outline-offset-2 focus:outline-amber-500 sm:text-sm"
+              />
+            </div>
+          </div>
 
-      <input
-        type="password"
-        placeholder="Password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
+          <div>
+            <div className="flex items-center justify-between">
+              <label htmlFor="password" className="block text-sm font-medium text-gray-100">
+                Password
+              </label>
+            </div>
+            <div className="mt-2">
+              <input
+                id="password"
+                name="password"
+                type="password"
+                required
+                autoComplete="current-password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                className="block w-full rounded-md bg-white/50 px-3 py-1.5 text-base text-white placeholder:text-gray-500 outline-1 -outline-offset-1 outline-white/10 focus:outline-2 focus:-outline-offset-2 focus:outline-amber-500 sm:text-sm"
+              />
+            </div>
+          </div>
 
-      <button>Create account</button>
-      <button onClick={handleGoogleLogin}>Login with Google</button>
-    </form>
+          <div>
+            <button
+              type="submit"
+              className="flex w-full justify-center rounded-md bg-indigo-500 px-3 py-1.5 text-sm font-semibold text-white hover:bg-indigo-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-amber-500"
+            >
+              Register
+            </button>
+          </div>
+        </form>
+
+        <div className="mt-4">
+          <button
+            onClick={handleGoogleLogin}
+            className="flex w-full justify-center rounded-md px-3 py-1.5 text-sm font-semibold text-white focus-visible:outline-2 focus-visible:outline-offset-2"
+          >
+            Sign in with Google
+          </button>
+        </div>
+
+        <p className="mt-10 text-center text-sm text-stone-200">
+          Already a member?{' '}
+          <a href="/login" className="font-semibold text-amber-900">
+            Login
+          </a>
+        </p>
+      </div>
+    </div>
   )
 }
