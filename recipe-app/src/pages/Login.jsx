220b4f5aa2cd68f220d82f4542787ec4dcbeb202
Bozhidar Zagorov <bozhidarzagorov@gmail.com>
2026-01-23 15:26:01 +0200

    fixing redirection on google outh

--- recipe-app/src/pages/Login.jsx
+++ recipe-app/src/pages/Login.jsx
@@ -34,6 +34,9 @@ export default function Login() {
   const handleGoogleLogin = async () => {
     const { error } = await supabase.auth.signInWithOAuth({
       provider: 'google',
+     options: {
+      redirectTo: window.location.origin,
+     },
     })
 
     if (error) {
--- recipe-app/src/pages/Register.jsx
+++ recipe-app/src/pages/Register.jsx
@@ -35,6 +35,9 @@ export default function Register() {
   const handleGoogleLogin = async () => {
     const { error } = await supabase.auth.signInWithOAuth({
       provider: 'google',
+     options: {
+      redirectTo: window.location.origin,
+     },
     })
     navigate('/')
 }
